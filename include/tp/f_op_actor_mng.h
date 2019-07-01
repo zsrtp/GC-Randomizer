cfedaf4dc652d96d38b10d7cf51b9cedadbd2fe2
Maurice Henke <aecx@aecx.cc>
2019-07-01 06:26:51 +0200

    unk1 to pos

--- include/tp/f_op_actor_mng.h
+++ include/tp/f_op_actor_mng.h
@@ -6,7 +6,7 @@ namespace tp::f_op_actor_mng {
 
 extern "C" {
 
-void createItemForTrBoxDemo(const float unk1[3], s32 item, 
+void createItemForTrBoxDemo(const float pos[3], s32 item, 
 	s32 unk3, s32 unk4, const float unk5[3], const float unk6[3]);
 
 }
