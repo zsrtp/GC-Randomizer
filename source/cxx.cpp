dac4fdd9dd4d6f7f595a0a00d39ff2d6b29d22a6
Zephiles <cae222@yahoo.com>
2019-07-05 17:17:16 -0500

    Added using std::size_t to defines.h

--- source/cxx.cpp
+++ source/cxx.cpp
@@ -1,14 +1,14 @@
+#include "defines.h"
+
 #include <tp/m_Do_ext.h>
 #include <tp/JKRExpHeap.h>
 
-#include <cstddef>
-
-void* operator new(std::size_t size)
+void* operator new(size_t size)
 {
 	void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
 	return tp::JKRExpHeap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
 }
-void* operator new[](std::size_t size)
+void* operator new[](size_t size)
 {
 	void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
 	return tp::JKRExpHeap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
