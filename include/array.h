7cc2117c8f01bc81306005ff7b63ca6a98f5810a
Maurice Henke <aecx@aecx.cc>
2019-07-06 16:12:13 +0200

    Header fix, param adjustments

--- include/array.h
+++ include/array.h
@@ -11,20 +11,20 @@ namespace mod::array
 		 * 
 		 * @param array 	Pointer to the array that's being shuffled
 		 * @param n 		Size of one element
-		 * @param length 	Number of elements within the array
+		 * @param count 	Number of elements within the array
 		 * @returns Nothing
 		 */
-		void shuffle(void* array, size_t n, size_t length);
+		void shuffle(void** array, size_t n, size_t count);
 
 		/**
-		 * Checks wether an array contains a value and returns it's index
+		 * Checks whether an array contains a value and returns it's index
 		 * 
 		 * @param needle 	The value to be searched for
 		 * @param haystack 	Pointer to the array
 		 * @param n 		Size of one element
-		 * @param length 	Number of elements within the array
+		 * @param count 	Number of elements within the array
 		 * @returns If successful the index of needle inside the array, otherwise -1
 		 */
-		s32 indexOf(void* needle, void* haystack, size_t n, size_t length);
+		s32 indexOf(void* needle, void** haystack, size_t n, size_t count);
 	}
 }
--- source/array.cpp
+++ source/array.cpp
@@ -1,16 +1,18 @@
 #include "array.h"
 #include "memory.h"
+
 #include <cstring>
 
 namespace mod::array
 {
-	void shuffle(void** array, size_t n, size_t length)
+	void shuffle(void** array, size_t n, size_t count)
 	{
 		// TODO
 	}
 
-	s32 indexOf(void* needle, void** haystack, size_t n, size_t length)
+	s32 indexOf(void* needle, void** haystack, size_t n, size_t count)
 	{
+		u32 length = count * n;
 		for (u32 i = 0; i < length; i += n)
 		{
 			if (memcmp(needle, haystack[i], n) == 0)
