196803d2a4f0b064903c170c60bc19f94e1861c8
Zephiles <cae222@yahoo.com>
2019-07-11 06:06:19 -0500

    Changed the name of the third parameter for do_alloc_JKRExpHeap
    The third parameter has different effects for different values, both positive and negative, so padding is not a proper name for it.

--- include/tp/JKRExpHeap.h
+++ include/tp/JKRExpHeap.h
@@ -6,7 +6,7 @@ namespace tp::jkr_exp_heap
 {
 	extern "C"
 	{
-		void* do_alloc_JKRExpHeap(void* heap, u32 size, s32 padding);
+		void* do_alloc_JKRExpHeap(void* heap, u32 size, s32 unk3);
 		void do_free_JKRExpHeap(void* heap, void* ptr);
 	}
 }
