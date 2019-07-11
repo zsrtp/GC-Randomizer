0d8184221d5f5cd1f844d02babe9cbffed27f38b
Zephiles <cae222@yahoo.com>
2019-07-11 06:22:46 -0500

    Adjusted the overloaded new operators to clear the memory that is allocated

--- source/cxx.cpp
+++ source/cxx.cpp
@@ -3,15 +3,21 @@
 #include <tp/m_Do_ext.h>
 #include <tp/JKRExpHeap.h>
 
+#include <cstring>
+
 void* operator new(size_t size)
 {
 	void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-	return tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
+	void* NewPtr = tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
+	memset(NewPtr, 0, size);
+	return NewPtr;
 }
 void* operator new[](size_t size)
 {
 	void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-	return tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
+	void* NewPtr = tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
+	memset(NewPtr, 0, size);
+	return NewPtr;
 }
 void operator delete(void* ptr)
 {
