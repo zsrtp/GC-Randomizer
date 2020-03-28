de982ec6bb379f2f8f926622d9e574bc9213e27d
Alex Bouchard <alexbouchard04@gmail.com>
2020-03-28 14:27:38 -0400

    fix error

--- source/mod.cpp
+++ source/mod.cpp
@@ -146,7 +146,7 @@ namespace mod
 		hudConsole->addOption(page, "Item slingshot", &chestRandomizer->itemThatReplacesSlingShot, 0xFF);*/
 		
 		hudConsole->addWatch(page, "CurrentStage:", &gameInfo.currentStage, 's', WatchInterpretation::_str);
-		hudConsole->addWatch(page, "CurrentRoom:", &tp::d_kankyo::env_light.currentRoom, 's', WatchInterpretation::_str);
+		hudConsole->addWatch(page, "CurrentRoom:", &tp::d_kankyo::env_light.currentRoom, 'd', WatchInterpretation::_u8);
 		
 		hudConsole->addWatch(page, "CurrentPosX:", &currentPosX, 's', WatchInterpretation::_str);
 		hudConsole->addWatch(page, "CurrentPosY:", &currentPosY, 's', WatchInterpretation::_str);
