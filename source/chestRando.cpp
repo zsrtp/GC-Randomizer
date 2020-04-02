a96e59ac92e860b85a3e01bcc2a9254fe762fe52
Alex Bouchard <alexbouchard04@gmail.com>
2020-04-02 14:21:57 -0400

    automatically reload when you get vessel of light

--- source/chestRando.cpp
+++ source/chestRando.cpp
@@ -424,12 +424,16 @@ namespace mod
 			gameInfo.scratchPad.wQuestLogData_2[0x28] = 16;
 			gameInfo.scratchPad.wQuestLogData_2[0x14F] |= 0x4;//give N faron warp(somehow doesn't work here?)
 			gameInfo.localAreaNodes.unk_0[0x8] = 0xFF;//give midna jumps in mist area
+			gameInfo.scratchPad.itemFlags.itemFlags3.Vessel_Of_Light_Faron = 0b1;//set flag for vessel since we'll skip it by reloading
+			gameInfo.nextStageVars.triggerLoad |= 1;
 			return item;
 		}
 		else if (item == items::Item::Vessel_Of_Light_Eldin)
 		{//set tear counter to 16
 			gameInfo.scratchPad.wQuestLogData_2[0x29] = 16;
 			gameInfo.scratchPad.wQuestLogData_2[0x16D] |= 0x20;//give death mountain warp(somehow doesn't work here?)
+			gameInfo.scratchPad.itemFlags.itemFlags3.Vessel_Of_Light_Eldin = 0b1;//set flag for vessel since we'll skip it by reloading
+			gameInfo.nextStageVars.triggerLoad |= 1;
 			return item;
 		}
 		else if (item == items::Item::Vessel_Of_Light_Lanayru)
@@ -437,6 +441,8 @@ namespace mod
 			gameInfo.scratchPad.wQuestLogData_2[0x2A] = 16;
 			gameInfo.scratchPad.wQuestLogData_2[0x18E] |= 0x4;//give lake hylia warp(somehow doesn't work here?)
 			gameInfo.scratchPad.wQuestLogData_2[0x1CF] |= 0x8;//give castle town warp(somehow doesn't work here?)
+			gameInfo.scratchPad.itemFlags.itemFlags3.Vessel_Of_Light_Eldin = 0b1;//set flag for vessel since we'll skip it by reloading
+			gameInfo.nextStageVars.triggerLoad |= 1;
 			return item;
 		}
 		
