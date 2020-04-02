2a0f2d939e4787559054c7c6d3597a4f32bedf82
Alex Bouchard <alexbouchard04@gmail.com>
2020-04-02 04:24:40 -0400

    fix problem with getting your first ooccoo outside FT

--- source/chestRando.cpp
+++ source/chestRando.cpp
@@ -454,6 +454,8 @@ namespace mod
 				// Correct stage
 				if(sourceCheck->itemID == item || (isItemBombs(item) && isItemBombs(sourceCheck->itemID)) ||
 				(item == items::Item::Red_Rupee && sourceCheck->itemID == items::Item::Giant_Bomb_Bag) ||
+				(item == items::Item::Ooccoo_Dungeon && sourceCheck->itemID == items::Item::Ooccoo_FT) ||
+				(item == items::Item::Ooccoo_FT && sourceCheck->itemID == items::Item::Ooccoo_Dungeon) ||
 				(item == items::Item::Lantern_Refill_Shop && sourceCheck->itemID == items::Item::Lantern_Oil_Shop) || 
 				(item == items::Item::Lantern_Refill_Scooped && sourceCheck->itemID == items::Item::Lantern_Oil_Scooped) || 
 				(sourceCheck->itemID == items::Item::Superb_Soup && (item == items::Item::Simple_Soup || item == items::Item::Good_Soup)))
