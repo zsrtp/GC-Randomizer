3e1357691e19cc9c7167bf25b10fbc44ace52120
Alex Bouchard <alexbouchard04@gmail.com>
2020-05-16 02:47:19 -0400

    make hawkeye and hylian shield appear only once they are supposed to

--- source/game_patches.cpp
+++ source/game_patches.cpp
@@ -302,29 +302,35 @@ namespace mod::game_patch
 	void handleMaloShop()
 	{
 		//hylian shield check
-		if (!tools::checkItemFlag(ItemFlags::Null_D9))
+		if ((gameInfo.scratchPad.eventBits[0xA] & 0x8) != 0)//KB1 done
 		{
-			gameInfo.localAreaNodes.unk_0[0xC] &= ~0x2;//unset flag for hylian shield bought
-			gameInfo.localAreaNodes.unk_0[0x13] |= 0x40;//set flag for hylian shield on counter
-		}
-		else 
-		{
-			gameInfo.localAreaNodes.unk_0[0xC] |= 0x2;//set flag for hylian shield bought
-			gameInfo.localAreaNodes.unk_0[0x13] &= ~0x40;//unset flag for hylian shield on counter
+			if (!tools::checkItemFlag(ItemFlags::Null_D9))
+			{
+				gameInfo.localAreaNodes.unk_0[0xC] &= ~0x2;//unset flag for hylian shield bought
+				gameInfo.localAreaNodes.unk_0[0x13] |= 0x40;//set flag for hylian shield on counter
+			}
+			else 
+			{
+				gameInfo.localAreaNodes.unk_0[0xC] |= 0x2;//set flag for hylian shield bought
+				gameInfo.localAreaNodes.unk_0[0x13] &= ~0x40;//unset flag for hylian shield on counter
+			}
 		}
 		
-		//hawkeye check		
-		if (!tools::checkItemFlag(ItemFlags::Null_D8))
-		{
-			gameInfo.localAreaNodes.unk_0[0xC] |= 0x40;//set flag for hawkeye on counter
-			gameInfo.localAreaNodes.unk_0[0xC] &= ~0x20;//unset flag for arrows on counter (else causes crash)
-			gameInfo.localAreaNodes.unk_0[0xD] &= ~0x8;//unset flag for hawkeye bought
-		}
-		else
-		{
-			gameInfo.localAreaNodes.unk_0[0xC] &= ~0x40;//unset flag for hawkeye on counter
-			gameInfo.localAreaNodes.unk_0[0xC] |= 0x20;//set flag for arrows on counter
-			gameInfo.localAreaNodes.unk_0[0xD] |= 0x8;//set flag for hawkeye bought
+		//hawkeye check	
+		if ((gameInfo.scratchPad.eventBits[0xEF] & 0x8) != 0)//Bow mini-game PoH gotten
+		{		
+			if (!tools::checkItemFlag(ItemFlags::Null_D8))
+			{
+				gameInfo.localAreaNodes.unk_0[0xC] |= 0x40;//set flag for hawkeye on counter
+				gameInfo.localAreaNodes.unk_0[0xC] &= ~0x20;//unset flag for arrows on counter (else causes crash)
+				gameInfo.localAreaNodes.unk_0[0xD] &= ~0x8;//unset flag for hawkeye bought
+			}
+			else
+			{
+				gameInfo.localAreaNodes.unk_0[0xC] &= ~0x40;//unset flag for hawkeye on counter
+				gameInfo.localAreaNodes.unk_0[0xC] |= 0x20;//set flag for arrows on counter
+				gameInfo.localAreaNodes.unk_0[0xD] |= 0x8;//set flag for hawkeye bought
+			}
 		}
 	}
 }
